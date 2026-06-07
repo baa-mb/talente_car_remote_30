1fbb9e547ac919fc533322547ce90dbcfd8c6bc2
baa <34665120+baa-mb@users.noreply.github.com>
2026-06-07 12:52:18 +0200

    sende_flag

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+function hole_neigung () {
+    gerade = Math.min(Math.max(input.rotation(Rotation.Pitch), -45), 45)
+    gerade = Math.round(gerade / k_empfind) * k_empfind
+    kurve = Math.min(Math.max(input.rotation(Rotation.Roll), -45), 45)
+    kurve = Math.round(kurve / k_empfind) * k_empfind
+    kurve = Math.round(kurve / empfind * 2) * empfind * 2
+}
 input.onButtonPressed(Button.B, function () {
     if (oben) {
         radio.sendValue("kupplung", 1)
@@ -6,15 +13,18 @@ input.onButtonPressed(Button.B, function () {
     }
     oben = !(oben)
 })
+let oben = false
 let kurve = 0
 let gerade = 0
-let oben = false
+let empfind = 0
+let k_empfind = 0
 let alt_gerade = -99
 let alt_kurve = -99
+let sende_flag = 0
 radio.setGroup(26)
 basic.showString("26")
 let g_empfind = 5
-let k_empfind = 5
+k_empfind = 5
 basic.showLeds(`
     . . . . .
     . . . . .
@@ -22,25 +32,24 @@ basic.showLeds(`
     . . . . .
     . . . . .
     `)
-let empfind = 4
+empfind = 4
 basic.forever(function () {
-    gerade = Math.min(Math.max(input.rotation(Rotation.Pitch), -45), 45)
-    gerade = Math.round(gerade / k_empfind) * k_empfind
-
-    kurve = Math.min(Math.max(input.rotation(Rotation.Roll), -45), 45)
-    kurve = Math.round(kurve / k_empfind) * k_empfind
-    kurve = Math.round(kurve / empfind * 2) * empfind * 2
-    if (gerade != alt_gerade || kurve != alt_kurve) {
-        // basic.pause(100)
-        if (input.buttonIsPressed(Button.A)) {
+    hole_neigung()
+    // basic.pause(100)
+    if (input.buttonIsPressed(Button.A)) {
+        if (gerade != alt_gerade || kurve != alt_kurve) {
             radio.sendValue("gerade", gerade)
             radio.sendValue("kurve", kurve / 3)
-        } else {
+            sende_flag = 1
+        }
+    } else {
+        if (sende_flag == 1) {
             radio.sendValue("gerade", 0)
             radio.sendValue("kurve", 0)
+            sende_flag = 0
         }
-        alt_gerade = gerade
-        alt_kurve = kurve
-        music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
     }
+    alt_gerade = gerade
+    alt_kurve = kurve
+    music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
 })
